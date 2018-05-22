7b60dbcfda6d588f4f2b3f2b071b91ccccdf717d
Jacquetta Bommuluri <jacquettavb@hotmail.com>
2018-05-22 04:09:32 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,10 +1,10 @@
-var playlist = {artistNames: 'song titles'};
+var playlist = {artistName: 'song titles'};
 
-function updatePlaylist(playlist, artistNames, songTitle) {
-  return Object.assign({}, playlist, {[artistNames]: songTitle})
+function updatePlaylist(playlist, artistName, songTitle) {
+  return Object.assign({}, playlist, {[artistName]: songTitle})
 }
 
-function removeFromPlaylist(playlist, artistNames){
-  delete artistNames.songTitle;
+function removeFromPlaylist(playlist, artistName){
+  delete artistName.songTitle;
   return playlist;
 }
